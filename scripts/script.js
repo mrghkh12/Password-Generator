0aa112188874e73e4bd00cc7008acbb2f3545f34
mohammad reza <mrghkh12@gmail.com>
2024-07-11 23:48:33 +0330

    feat(script.js): create random password generator function

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,20 @@ const numbersZeroToNine = '0123456789';
 const selectedSpecialCharacters = '!@#$%^&*()+-.~|<>[],=-_/:;?{}~';
 const passwordLenght = 10
 
+const outputPassword = $.querySelector('#outputPassword')
+const generatorbtn = $.querySelector(".outputBtn")
+
 let allCharacters = lowerCaseLetters + upperCaseLetters + numbersZeroToNine + selectedSpecialCharacters
 
 let password = ''
 
+generatorbtn.addEventListener('click' , passwordGenerator)
+function passwordGenerator(){
+    outputPassword.value = ''
+    password = ''
+    while(password.length < passwordLenght){
+        password += allCharacters[Math.round(Math.random() * allCharacters.length)]
+    }
+    console.log(password);
+    outputPassword.value = password
+}
