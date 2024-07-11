52b61585185196a4375fd8e9c866bad1770b13b8
mohammad reza <mrghkh12@gmail.com>
2024-07-11 23:41:12 +0330

    feat(script.js): create sturcture codes

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const $ =document
+
+const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbersZeroToNine = '0123456789';
+const selectedSpecialCharacters = '!@#$%^&*()+-.~|<>[],=-_/:;?{}~';
+const passwordLenght = 10
+
+let allCharacters = lowerCaseLetters + upperCaseLetters + numbersZeroToNine + selectedSpecialCharacters
+
+let password = ''
+
